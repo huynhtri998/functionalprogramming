88a44abf11bee4f24a490b9c3c26ace65123a21d
huynhtri998 <huynhtri998@gmail.com>
2025-10-22 19:46:51 +0700

    functional programming

--- src/Sorted/Main.java
+++ src/Sorted/Main.java
@@ -3,14 +3,15 @@ package Sorted;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Main {
 
     private static List<Student> getStudents() {
         List<Student> students = new ArrayList<>();
-        students.add(new Student("James",20,8.0));
-        students.add(new Student("Anne",22,9.0));
+        students.add(new Student("James",22,8.0));
+        students.add(new Student("Anne",20,9.0));
         students.add(new Student("Mary",21,5.0));
         students.add(new Student("David",18,7.0));
         students.add(new Student("Dave",19,6.0));
@@ -19,12 +20,13 @@ public class Main {
 
     static void main() {
         List<Student> students = getStudents();
-        List<Student> fillteredStudent =
+        long i =
                 students
                         .stream()
                         .filter(student -> student.getScore() > 7.0)
                         .sorted((Comparator.comparing(Student::getName).thenComparing(Student::getAge).reversed()))
-                        .toList();
-        System.out.println("Student List:" + fillteredStudent);
+                        .map(student -> new StudentDTO(student.getName(), student.getAge(), student.getScore()))
+                        .count();
+        System.out.println("Student List:" + i);
     }
 }
---
+++ src/Sorted/StudentDTO.java
@@ -0,0 +1,44 @@
+package Sorted;
+
+public class StudentDTO {
+    private String name;
+    private int age;
+    private double score;
+
+    StudentDTO() {}
+
+    public StudentDTO(String name, int age, double score) {
+        this.name = name;
+        this.age = age;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return  "StudentDTO{" + "name=" + name + ", age=" + age + ", score=" + score + '}';
+    }
+}
